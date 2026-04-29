dc292d7a6c4b1c7f0be1aee00ba15d73e4c1b78d
Salem Abd El Rahman <salemkamal977@gmail.com>
2026-04-30 02:51:42 +0300

    fix_server_rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'Question/:examid',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'Exam/:id/:title',
+    renderMode: RenderMode.Server
+  },
+   {
+    path: 'Answer/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
